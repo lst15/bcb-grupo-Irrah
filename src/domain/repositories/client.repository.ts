2e2a87ffd0a552c3156c5e2e9c41536a50827bea
Leandro Salustriano <leandro.salustriano@hotmail.com>
2023-09-15 15:45:26 -0300

    added more functions

--- src/domain/repositories/client.repository.ts
+++ src/domain/repositories/client.repository.ts
@@ -1,4 +1,10 @@
 import { ClientModel } from '../models/client.model';
 import { BaseRepository } from './base.repository';
 
-export abstract class ClientRepository extends BaseRepository<ClientModel> {}
+export abstract class ClientRepository extends BaseRepository<ClientModel> {
+  abstract includeCredits(credits: number): Promise<ClientModel | null>;
+  abstract checkBalance(): Promise<number | null>;
+  abstract changeLimit(): Promise<ClientModel | null>;
+  abstract changePlain(): Promise<ClientModel | null>;
+  abstract getClient(cpf: string): Promise<ClientModel | null>;
+}
