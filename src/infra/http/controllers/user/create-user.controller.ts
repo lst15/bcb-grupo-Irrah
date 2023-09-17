318e96771d49a06a8f6b330b546af56938e8e4b0
Leandro Salustriano <leandro.salustriano@hotmail.com>
2023-09-17 20:56:10 -0300

    added user types

--- src/infra/http/controllers/user/create-user.controller.ts
+++ src/infra/http/controllers/user/create-user.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Body } from '@nestjs/common';
 // import { UserModel } from 'src/domain/models/user.model';
 import { CreateUserUseCase } from 'src/domain/usecases/user/create-user.usecase';
 import { CreateUserDto } from '../../dtos/users/create-user.dto';
+import { UserModel } from 'src/domain/models/user.model';
 
 @Controller('users')
 export class CreateUserController {
@@ -9,7 +10,8 @@ export class CreateUserController {
 
   @Post()
   async handle(@Body() user: CreateUserDto) {
-    const executed = await this.createUserUseCase.execute(user);
+    const executed: object | UserModel =
+      await this.createUserUseCase.execute(user);
 
     return {
       success: true,
